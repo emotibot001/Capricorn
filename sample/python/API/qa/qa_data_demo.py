9a18bae305938774127df7ee67ee2ded84418e02
gjy <gjy@emotibotdeMacBook-Pro-8.local>
2020-11-17 18:33:03 +0800

    add python folder

---
+++ sample/python/API/qa/qa_data_demo.py
@@ -0,0 +1,54 @@
+import bf_engine
+import json
+
+# 机器人创建
+bot = bf_engine.init()
+
+# 机器人appid
+print("appid: " + bot.app_id)
+
+# 机器人数据
+qalist  = bot.qa.qa_list()
+
+print("语料列表： " + str(json.dumps(qalist, ensure_ascii=False, indent=2)))
+
+print('qa出话：' + str(bot.qa.query('竹间你好呀')))
+
+# 更新问题
+question_id = bot.qa.update_question(
+    {
+        "id": 37,
+        "sq": "谁是NLP宇宙第一",
+        "tags": ["你好呢"],
+        "lq": ["竹间你好呀", "竹间你好吗", "你好呀"],
+        "answers": [
+            {"answer": "未来竹间是NLP NO1", "time_tag": "NLP混沌时间"},
+            {"answer": "竹间是NLP NO1", "time_tag": "竹间成立时间"}
+        ]
+    }
+)
+# 训练机器人
+bot.qa.train(data={"data": []}, append=True)
+print('qa出话：' + str(bot.qa.query('竹间你好呀')))
+
+# 删除问题
+print("删除一条标准问题")
+bot.qa.delete_question(question_id)
+# 机器人数据
+print("语料列表： " + str(json.dumps(bot.qa.qa_list(), ensure_ascii=False, indent=2)))
+
+# 添加问题
+print("添加一条标准问题")
+bot.qa.add_question(
+    {
+        "sq": "谁是NLP宇宙第一",
+        "tags": ["你好呢"],
+        "lq": ["竹间你好呀", "竹间你好吗", "你好呀"],
+        "answers": [
+            {"answer": "未来竹间是NLP NO1", "time_tag": "NLP混沌时间"},
+            {"answer": "竹间是NLP NO1", "time_tag": "竹间成立时间"}
+        ]
+    }
+)
+# 机器人数据
+print("语料列表： " + str(json.dumps(bot.qa.qa_list(), ensure_ascii=False, indent=2)))
--- sample/python/API/qa/qa_data_demo_train.py
+++ sample/python/API/qa/qa_data_demo_train.py
@@ -8,30 +8,15 @@ bot = bf_engine.init()
 print("appid: " + bot.app_id)
 
 # 机器人数据
-print("语料列表： " + str(json.dumps(bot.qa.qa_list(), ensure_ascii=False, indent=2)))
+qalist  = bot.qa.qa_list()
 
-print('qa出话：' + str(bot.qa.query('竹间你好呀')))
+print("语料列表： " + str(json.dumps(qalist, ensure_ascii=False, indent=2)))
 
-# 更新问题
-question_id = bot.qa.update_question(
-    {
-        "id": 37,
-        "sq": "谁是NLP宇宙第一",
-        "tags": ["你好呢"],
-        "lq": ["竹间你好呀", "竹间你好吗", "你好呀"],
-        "answers": [
-            {"answer": "未来竹间是NLP NO1", "time_tag": "NLP混沌时间"},
-            {"answer": "竹间是NLP NO1", "time_tag": "竹间成立时间"}
-        ]
-    }
-)
-# 训练机器人
-bot.qa.train(data={"data": []}, append=True)
 print('qa出话：' + str(bot.qa.query('竹间你好呀')))
 
 # 删除问题
 print("删除一条标准问题")
-bot.qa.delete_question(question_id)
+bot.qa.delete_question()
 # 机器人数据
 print("语料列表： " + str(json.dumps(bot.qa.qa_list(), ensure_ascii=False, indent=2)))
 
